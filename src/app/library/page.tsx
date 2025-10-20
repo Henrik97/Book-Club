53bd8711429bacd4fd3c654334228a0a910138e6
Henrik Woodgates <henrikwoodgates97@gmail.com>
2025-10-20 22:24:14 +0200

    base fo the project

---
+++ src/app/library/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+import BookCard from "@/components/BookCard";
+import { useState, useEffect } from "react";
+import { supabase } from "../util/superbase/client";
+
+interface Book {
+  id: string;
+  title: string;
+  author: string;
+  image_url: string | null;
+  description?: string | null;
+}
+
+export default function Library() {
+  const [books, setBooks] = useState<Book[]>([]);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const { data, error } = await supabase.from("books").select("*");
+      if (error) {
+        console.error("Error fetching books:", error.message);
+      } else {
+        setBooks(data || []);
+      }
+    };
+
+    fetchBooks();
+  }, []);
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+      {books.map((book) => (
+        <BookCard
+          key={book.id}
+          title={book.title}
+          author={book.author}
+          // imageUrl={book.image_url || undefined}
+        />
+      ))}
+    </div>
+  );
+}
---
+++ src/components/BookCard.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import { Card, CardDescription, CardHeader, CardTitle } from "./ui/card";
+interface Props {
+  title: string;
+  author: string;
+  imageUrl: string;
+  onClick: () => void;
+}
+export default function BookCard({ title, author, imageUrl, onClick }: Props) {
+  return (
+    <Card onClick={onClick}>
+      <CardHeader>
+        {imageUrl && (
+          <div className="relative w-full h-56 mb-2">
+            <Image
+              src={imageUrl}
+              alt={title}
+              fill
+              className="object-cover rounded-md"
+            />
+          </div>
+        )}
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{author}</CardDescription>
+      </CardHeader>
+    </Card>
+  );
+}
